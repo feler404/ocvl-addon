0e4431c77e1db75819898e48ab9de3fa14e1946c
Dawid Anioł <dawid.aniol@grandparade.co.uk>
2018-04-17 14:28:16 +0200

    move helop operator to community

--- operatores.py
+++ operatores.py
@@ -79,14 +79,55 @@ class OCVLImageFullScreenOperator(bpy.types.Operator):
         return {'RUNNING_MODAL'}
 
 
+class OCVLShowTextInTextEditorOperator(bpy.types.Operator):
+    bl_idname = "text.show_help_in_text_editor"
+    bl_label = "OCVL show help in text editor"
+
+    origin = StringProperty("")
+
+    def modal(self, context, event):
+
+        if event.type in {'ESC'}:
+
+            bpy.context.area.type = "NODE_EDITOR"
+            if context.window.screen.show_fullscreen:
+                bpy.ops.screen.back_to_previous()
+            if 'TEXT_INPUT_NODE' in bpy.data.texts.keys():
+                bpy.data.texts.remove(bpy.data.texts["TEXT_INPUT_NODE"])
+            return {'FINISHED'}
+
+        return {'PASS_THROUGH'}
+
+    def invoke(self, context, event):
+        context.window_manager.modal_handler_add(self)
+        bpy.context.area.type = "TEXT_EDITOR"
+        node_tree, node_name, *props_name = self.origin.split('|><|')
+        self.node = bpy.data.node_groups[node_tree].nodes[node_name]
+
+        if node_name not in bpy.data.texts.keys():
+            bpy.data.texts.new(node_name)
+            text = bpy.data.texts[node_name]
+            doc = getattr(self.node, '__doc__', None) or 'Lack of offline documentation.'
+            text.write(doc)
+
+        self.text = context.space_data.text = bpy.data.texts[node_name]
+        context.space_data.show_line_numbers = True
+        context.space_data.show_word_wrap = True
+        context.space_data.show_syntax_highlight = True
+        context.space_data.font_size = 16
+        bpy.ops.screen.screen_full_area()
+
+        return {'RUNNING_MODAL'}
 
 
 
 def register():
     bpy.utils.register_class(OCVLImageFullScreenOperator)
     bpy.utils.register_class(EscapeFullScreenOperator)
+    bpy.utils.register_class(OCVLShowTextInTextEditorOperator)
 
 
 def unregister():
+    bpy.utils.unregister_class(OCVLShowTextInTextEditorOperator)
     bpy.utils.unregister_class(EscapeFullScreenOperator)
     bpy.utils.unregister_class(OCVLImageFullScreenOperator)
