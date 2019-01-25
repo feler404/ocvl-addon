c0f24fcb760d8cfbe5e8cebe8c32f5a17563d28e
Dawid Aniol <dawid.aniol@teredo.tech>
2019-01-25 18:21:59 +0100

    pre make step 2

--- build_files/pre_make.py
+++ build_files/pre_make.py
@@ -39,31 +39,70 @@ def cmd(bash_cmd):
 
 # -----  Settings
 BLENDER_BUILD_DIR_NAME = "blender-build"
-BLENDER_BUILD_DIR = get_blender_build_path(BLENDER_BUILD_DIR_NAME)
+BLENDER_BUILD_DIR = os.path.normpath(get_blender_build_path(BLENDER_BUILD_DIR_NAME))
 OCVL_ADDON_DIR = "ocvl-addon"
 BLENDER_SOURCE_DIR = "blender"
-GETPIP_URL = "https://bootstrap.pypa.io/get-pip.py"
+GET_PIP_FILE_NAME = "get-pip.py"
+GET_PIP_URL = f"https://bootstrap.pypa.io/{GET_PIP_FILE_NAME}"
 OCVL_REQUIREMENTS_PATH = os.path.join(BLENDER_BUILD_DIR, OCVL_ADDON_DIR, "requirements.txt")
+WORK_DIR = os.getcwd()
 
 
+def update_blender(branch='master'):
+    """
+    Update Blender
+    :return:
+    """
+    os.chdir(os.path.join(BLENDER_BUILD_DIR, BLENDER_SOURCE_DIR))
+    cmd(f"git resert --hard")
+    cmd(f"git checkout {branch}")
+    cmd(f"git pull")
+    os.chdir(WORK_DIR)
 
-def updaet_ocvl_addon():
+
+def update_blender_submodule(branch='master'):
+    """
+    Update Blender submodules
+    :param branch:
+    :return:
+    """
+    os.chdir(os.path.join(BLENDER_BUILD_DIR, BLENDER_SOURCE_DIR))
+    cmd(f"git submodule foreach git checkout {branch}")
+    cmd(f"git submodule foreach git pull --rebase origin {branch}")
+    os.chdir(WORK_DIR)
+
+
+def update_ocvl_addon(branch='master'):
     """
     Update OCVL ADDON
     :return:
     """
-    cmd(f"cd {$BLENDER_BUILD_DIR}/{OCVL_ADDON_DIR}")
+    os.chdir(os.path.join(BLENDER_BUILD_DIR, OCVL_ADDON_DIR))
     cmd(f"git resert --hard")
-    cmd(f"git checkout master")
+    cmd(f"git checkout {branch}")
     cmd(f"git pull")
-    cmd(f"cd -")
+    os.chdir(WORK_DIR)
 
 
-# if not os.path.isfile(BLENDER_PYTHON_PIP_BIN):
-#     # ----- GetPIP download
-#     cmd(f"curl {GETPIP_URL} -o get-pip.py")
-#     get_pip_path = os.path.join(os.getcwd(), "get-pip.py")
-#     cmd(f"{BLENDER_PYTHON_BIN} {get_pip_path}")
+def build_blender(version="lite"):
+    os.chdir(os.path.join(BLENDER_BUILD_DIR, BLENDER_SOURCE_DIR))
+    print(os.listdir(os.path.join(BLENDER_BUILD_DIR, BLENDER_SOURCE_DIR)))
+    cmd(f"make.bat clean")
+    cmd(f"make.bat {version}")
+    os.chdir(WORK_DIR)
+
+
+def get_get_pip_script():
+    """
+    Get get-pip file from remote server
+    :return:
+    """
+    if not os.path.isfile("{}".format(os.path.join(BLENDER_BUILD_DIR, GET_PIP_FILE_NAME))):
+        # ----- GetPIP download
+        # cmd(f"cd {BLENDER_BUILD_DIR}")
+        cmd(f"curl {GET_PIP_URL} -o {GET_PIP_FILE_NAME}")
+        # get_pip_path = os.path.join(os.getcwd(), "get-pip.py")
+        # cmd(f"{BLENDER_PYTHON_BIN} {get_pip_path}")
 
 
 # # ----- Install requirements
@@ -73,3 +112,12 @@ def updaet_ocvl_addon():
 # OCVL_RELEASE_DIR = os.path.join(BLENDER_BUILD_DIR, OCVL_ADDON_DIR, "build_files", "release") + "/"
 # BLENDER_RELEASE_DIR = os.path.join(BLENDER_BUILD_DIR, BLENDER_SOURCE_DIR, "release")
 # cmd("cp -Rfv {} {}".format(OCVL_RELEASE_DIR, BLENDER_RELEASE_DIR))
+
+
+
+try:
+    # update_blender(branch="blender2.8")
+    # update_blender_submodule()
+    build_blender()
+finally:
+    os.chdir(WORK_DIR)
