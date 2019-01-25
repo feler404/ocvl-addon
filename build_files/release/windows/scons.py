ae5f228a800f3829cc1873b0bb540e46c768d989
Dawid Aniol <dawid.aniol@teredo.tech>
2019-01-25 10:30:52 +0100

    check keys #2

--- build_files/release/windows/scons.py
+++ build_files/release/windows/scons.py
@@ -54,7 +54,6 @@ def nsis_installer(target=None, source=None, env=None):
     # change to suit install dir
     inst_dir = install_base_dir + env['BF_INSTALLDIR']
 
-
     os.chdir(rel_dir)
 
     ns = open("00.sconsblender.nsi", "r")
