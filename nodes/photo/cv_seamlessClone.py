44d9dbcf48ccfaee22d0242193018530d0328426
Dawid Aniol <dawid.aniol@teredo.tech>
2019-06-20 17:30:27 +0200

    update seamleessClone node

--- nodes/photo/cv_seamlessClone.py
+++ nodes/photo/cv_seamlessClone.py
@@ -8,21 +8,23 @@ from ocvl.core.node_base import OCVLNodeBase, update_node
 
 class OCVLseamlessCloneNode(OCVLNodeBase):
 
-    n_doc = """
-    Image editing tasks concern either global changes (color/intensity corrections, filters, deformations) or local changes concerned to a selection. Here we are interested in achieving local changes, ones that are restricted to a region manually selected (ROI), in a seamless and effortless manner. The extent of the changes ranges from slight distortions to complete replacement by novel content
-    """
     bl_flags_list = 'NORMAL_CLONE, MIXED_CLONE, FEATURE_EXCHANGE'
+    n_doc = "Image editing tasks concern either global changes (color/intensity corrections, filters, deformations) or local changes concerned to a selection. Here we are interested in achieving local changes, ones that are restricted to a region manually selected (ROI), in a seamless and effortless manner. The extent of the changes ranges from slight distortions to complete replacement by novel content"
+    n_quick_link_requirements = {
+        "src_in": {"width_in": 100, "height_in": 100},
+        "dst_in": {"width_in": 300, "height_in": 300},
+    }
 
     src_in: bpy.props.StringProperty(name="src_in", default=str(uuid.uuid4()), description="Input 8-bit 3-channel image.")
     dst_in: bpy.props.StringProperty(name="dst_in", default=str(uuid.uuid4()), description="Input 8-bit 3-channel image.")
     mask_in: bpy.props.StringProperty(name="mask_in", default=str(uuid.uuid4()), description="Input 8-bit 1 or 3-channel image.")
-    p_in: bpy.props.IntVectorProperty(default=(0, 0), size=2, update=update_node, description="Point in dst image where object is placed.")
+    p_in: bpy.props.IntVectorProperty(default=(50, 50), size=2, update=update_node, description="Point in dst image where object is placed.")
     flags_in: bpy.props.BoolVectorProperty(default=[False for i in bl_flags_list.split(",")], size=len(bl_flags_list.split(",")), update=update_node, subtype="NONE", description=bl_flags_list)
 
     result_out: bpy.props.StringProperty(name="result_out", default=str(uuid.uuid4()), description="Output image with the same size and type as dst.")
 
     def init(self, context):
-        self.inputs.new("StringsSocket", "src_in")
+        self.inputs.new("ImageSocket", "src_in")
         self.inputs.new('ImageSocket', "dst_in")
         self.inputs.new('StringsSocket', "mask_in")
         self.inputs.new('StringsSocket', "p_in").prop_name = 'p_in'
