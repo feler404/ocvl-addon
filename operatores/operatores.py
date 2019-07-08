47680ab75b4633e52708de776c5cf249cfe2662e
DawidAniol <daniol@strikesocial.com>
2019-07-08 18:42:21 +0200

    [OCVL-151] remove duplicated operatores

--- operatores/operatores.py
+++ operatores/operatores.py
@@ -13,93 +13,6 @@ logger = logging.getLogger(__name__)
 TUTORIAL_HEARTBEAT_INTERVAL_RTSP_REFRESH = 2
 
 
-class OCVLImageFullScreenOperator(bpy.types.Operator):
-    bl_idname = "image.image_full_screen"
-    bl_label = "OCVL Image Full Screen"
-
-    origin = bpy.props.StringProperty("")
-
-    def modal(self, context, event):
-        if event.type in {'ESC'}:
-            return self._exit(context, exit_mode='CANCELLED')
-
-        return {'PASS_THROUGH'}
-
-    def _load_np_img_to_blender_data_image(self, img_name, img_data):
-        bl_img = bpy.data.images.get(img_name)
-        if bl_img:
-            return bl_img
-        gl_img_data = convert_to_gl_image(img_data)
-        height, width = img_data.shape[:2]
-        bl_img = bpy.data.images.new(img_name, width, height)
-        bl_img.pixels = list(gl_img_data.flat)
-        return bl_img
-
-    def _exit(self, context, exit_mode='FINISHED'):
-        bpy.context.area.type = "NODE_EDITOR"
-        if context.window.screen.show_fullscreen:
-            bpy.ops.screen.back_to_previous()
-        return {exit_mode}
-
-    def invoke(self, context, event):
-        self.points = []
-        self.points = []
-        node_tree, node_name, *props_name = self.origin.split('|><|')
-        self.node = node = bpy.data.node_groups[node_tree].nodes[node_name]
-        self.props_name = props_name
-        self.props_counter = 0
-        if node.inputs["image_in"].is_linked:
-
-            try:
-                img_data = node.get_from_props("image_in")
-                img_name = node.inputs.get("image_in").sv_get()
-            except NoDataError as e:
-                return {'CANCELLED'}
-            bl_img = self._load_np_img_to_blender_data_image(img_name, img_data)
-        else:
-            return {'CANCELLED'}
-
-        context.window_manager.modal_handler_add(self)
-        bpy.context.area.type = "IMAGE_EDITOR"
-        for area in bpy.context.screen.areas:
-            if area.type == 'IMAGE_EDITOR':
-                area.spaces.active.image = bl_img
-        bpy.context.area.type = "IMAGE_EDITOR"
-        bpy.ops.image.view_all(fit_view=True)
-        bpy.ops.screen.screen_full_area()
-
-        args = (self, context)
-
-        return {'RUNNING_MODAL'}
-
-
-class OCVLImageImporterOperator(bpy.types.Operator):
-    bl_idname = "image.image_importer"
-    bl_label = "Open Image"
-    bl_options = {'REGISTER'}
-
-    filepath = bpy.props.StringProperty(
-        name="File Path",
-        description="Filepath used for importing the font file",
-        maxlen=1024, default="", subtype='FILE_PATH')
-
-    origin = bpy.props.StringProperty("")
-
-    def execute(self, context):
-        node_tree, node_name = self.origin.split('|><|')
-        node = bpy.data.node_groups[node_tree].nodes[node_name]
-        node.loc_filepath = self.filepath
-        node.loc_name_image = ''
-        node.process()
-        return {'FINISHED'}
-
-    def invoke(self, context, event):
-        wm = context.window_manager
-        wm.fileselect_add(self)
-        return {'RUNNING_MODAL'}
-
-
-
 class EscapeFullScreenOperator(bpy.types.Operator):
     """Tooltip"""
     bl_idname = "screen.escape_full_screen"
@@ -381,7 +294,6 @@ def register():
     ocvl_register(OCVLImageFullScreenOperator)
     ocvl_register(OCVLImageImporterOperator)
 
-    ocvl_register(OCVLImageFullScreenOperator)
     ocvl_register(EscapeFullScreenOperator)
     ocvl_register(OCVLShowTextInTextEditorOperator)
     ocvl_register(OCVLClearDeskOperator)
@@ -389,16 +301,11 @@ def register():
     ocvl_register(OCVLChangeThemeLightOperator)
     ocvl_register(OCVLChangeThemeDarkOperator)
     ocvl_register(OCVLShowNodeSplashOperator)
-    ocvl_register(OCVLImageImporterOperator)
     ocvl_register(OCVLGeneratePythonCodeOperator)
 
 
 def unregister():
-    ocvl_unregister(OCVLImageFullScreenOperator)
-    ocvl_unregister(OCVLImageImporterOperator)
-
     ocvl_unregister(OCVLGeneratePythonCodeOperator)
-    ocvl_unregister(OCVLImageImporterOperator)
     ocvl_unregister(OCVLShowNodeSplashOperator)
     ocvl_unregister(OCVLChangeThemeDarkOperator)
     ocvl_unregister(OCVLChangeThemeLightOperator)
@@ -406,4 +313,5 @@ def unregister():
     ocvl_unregister(OCVLClearDeskOperator)
     ocvl_unregister(OCVLShowTextInTextEditorOperator)
     ocvl_unregister(EscapeFullScreenOperator)
+    ocvl_unregister(OCVLImageImporterOperator)
     ocvl_unregister(OCVLImageFullScreenOperator)
