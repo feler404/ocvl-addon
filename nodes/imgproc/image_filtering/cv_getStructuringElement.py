d7343fbb76946eac767868ac3037465e22622675
aadankan <aadankan@gmail.com>
2019-08-01 14:16:42 +0200

    [OCVL-195]

--- nodes/imgproc/image_filtering/cv_getStructuringElement.py
+++ nodes/imgproc/image_filtering/cv_getStructuringElement.py
@@ -17,20 +17,24 @@ class OCVLgetStructuringElementNode(OCVLNodeBase):
     def get_ksize(self):
         return self.get("ksize_in", 5)
 
+    shape_in: bpy.props.EnumProperty()#Co tu wwpisać do nawiasu?
+    anchor_in: bpy.props.IntVectorProperty(default=(-1, -1), update=update_node, size=2, description="Position of the anchor within the element.")
     ksize_in: bpy.props.IntProperty(default=5, update=update_node, min=1, max=30, get=get_ksize, set=set_ksize,description="Size of the structuring element.")
 
     def init(self, context):
         self.inputs.new('StringsSocket', "ksize_in").prop_name = 'ksize_in'
+        self.inputs.new('StringsSocket', "anchor_in").prop_name = 'anchor_in'
 
         self.outputs.new("ImageSocket", "retval_out")
 
     def wrapped_process(self):
         kwargs = {
             'ksize_in': self.get_from_props("ksize_in"),
+            'anchor_in': self.get_from_props("anchor_in")
         }
 
-        dst_out = self.process_cv(fn=cv2.getStructuringElement, kwargs=kwargs)
-        self.refresh_output_socket("retval_output", dst_out, is_uuid_type=True)
+        retval_out = self.process_cv(fn=cv2.getStructuringElement, kwargs=kwargs)
+        self.refresh_output_socket("retval_output", retval_out, is_uuid_type=True)
 
     def draw_buttons(self, context, layout):
         self.add_button(layout, 'borderType_in')
