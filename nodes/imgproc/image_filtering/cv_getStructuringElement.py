7af6f721d28cabd2afb9c595b7288f53156608ac
aadankan <aadankan@gmail.com>
2019-08-01 18:13:47 +0200

    [OCVL-195]-3

--- nodes/imgproc/image_filtering/cv_getStructuringElement.py
+++ nodes/imgproc/image_filtering/cv_getStructuringElement.py
@@ -9,6 +9,13 @@ class OCVLgetStructuringElementNode(OCVLNodeBase):
     n_note = "When using OpenCV 1.x C API, the created structuring element IplConvKernel* element must be released in the end using cvReleaseStructuringElement(&element)."
     n_requirments = {}
 
+    SDEPTH_ITEMS = (
+    ("None", "None", "None", "", 0),
+    ("CV_32S", "CV_32S", "CV_32S", "", 1),
+    ("CV_32F", "CV_32F", "CV_32F", "", 2),
+    ("CV_64F", "CV_64F", "CV_64F", "", 3),
+    ("CV_32S", "CV_32S", "CV_32S", "", 1))
+
     def set_ksize(self, value):
         if value % 2 == 0:
             value = value + 1
@@ -17,25 +24,28 @@ class OCVLgetStructuringElementNode(OCVLNodeBase):
     def get_ksize(self):
         return self.get("ksize_in", 5)
 
-    shape_in: bpy.props.EnumProperty()#Co tu wwpisać do nawiasu?
+    shape_in: bpy.props.EnumProperty(items = SDEPTH_ITEMS,default = 3,update = update_node)
     anchor_in: bpy.props.IntVectorProperty(default=(-1, -1), update=update_node, size=2, description="Position of the anchor within the element.")
     ksize_in: bpy.props.IntProperty(default=5, update=update_node, min=1, max=30, get=get_ksize, set=set_ksize,description="Size of the structuring element.")
 
     def init(self, context):
         self.inputs.new('StringsSocket', "ksize_in").prop_name = 'ksize_in'
         self.inputs.new('StringsSocket', "anchor_in").prop_name = 'anchor_in'
+        self.inputs.new('StringSocket', "shape_in")
 
         self.outputs.new("ImageSocket", "retval_out")
 
     def wrapped_process(self):
         kwargs = {
             'ksize_in': self.get_from_props("ksize_in"),
-            'anchor_in': self.get_from_props("anchor_in")
+            'anchor_in': self.get_from_props("anchor_in"),
+            'shape_in': self.get_from_props("shape_in"),
         }
 
         retval_out = self.process_cv(fn=cv2.getStructuringElement, kwargs=kwargs)
         self.refresh_output_socket("retval_output", retval_out, is_uuid_type=True)
 
     def draw_buttons(self, context, layout):
+        self.add_button(layout, 'borderType_in')
         self.add_button(layout, 'borderType_in')
         self.add_button(layout, 'borderType_in')
